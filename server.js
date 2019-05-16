3279cc3ef63afb20a22cf057a515dd637c012591
ShererStress <ryanswhitehill@gmail.com>
2019-05-16 17:19:15 -0600

    item controller and item model created and tested

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 // middlewear
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
 
 // connect to mongoose
 mongoose.connect('mongodb://localhost:27017/project_three', { useNewUrlParser: true });
@@ -17,3 +18,10 @@ mongoose.connection.once('open', () => {
 app.listen(3000, () => {
   console.log('listening...');
 });
+
+//Routers
+const itemController = require(`./controllers/item_controller.js`);
+
+//Routes
+
+app.use(`/items`, itemController);
