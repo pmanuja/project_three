bf202a239d2c6fbc0742a97f21411e19495fce18
Jon Phillips <jonphillips@Jons-MacBook-Pro.local>
2019-05-16 15:44:54 -0500

    built basic file structure, connected angular, mongoose, express

---
+++ server.js
@@ -0,0 +1,19 @@
+// dependencies
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// middlewear
+app.use(express.json());
+app.use(express.static('public'));
+
+// connect to mongoose
+mongoose.connect('mongodb://localhost:27017/project_three', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('i am a mongoose');
+});
+
+// port listening...
+app.listen(3000, () => {
+  console.log('listening...');
+});
