064a49d261b44329807a6616ab1ef53819e4df9c
Pooja Manuja <poojamanuja@Poojas-MBP.attlocal.net>
2019-05-17 15:49:43 -0400

    Added env file and upadted server.js file accordingly

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const db = mongoose.connection;
+require('dotenv').config();
+
 // middleware
 app.use(express.json());
 app.use(express.static('public'));
@@ -12,11 +15,27 @@ app.use(express.urlencoded({ extended: false }));
 const userController = require('./controllers/user.js');
 const itemController = require(`./controllers/item_controller.js`);
 
-// connect to mongoose
-mongoose.connect('mongodb://localhost:27017/project_three', { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-  console.log('i am a mongoose');
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log(MONGODB_URI);
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// open the connection to mongo
+db.on('open' , ()=>{console.log('i am a mongoose');
 });
+
+// // connect to mongoose
+// mongoose.connect(, { useNewUrlParser: true });
+// mongoose.connection.once('open', () => {
+//   console.log('i am a mongoose');
+// });
 
 // port listening...
 app.listen(3000, () => {
