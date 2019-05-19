e8914871fc3a6b0a6c931ca181f3b8313059abbf
Pooja Manuja <poojamanuja@Poojas-MBP.attlocal.net>
2019-05-19 12:53:41 -0400

    added seed data and updated storefront partial to display image and desc

--- models/seedProducts.js
+++ models/seedProducts.js
@@ -4,55 +4,55 @@ module.exports = [
     price: 15,
     category: "Electronics",
     description: 'Cnacnoo Ultra-Slim Keyboard for PC Computer Laptop, Wired USB Small Keyboards with 78 Keys, Portable Multimedia Mini Keyboards Universal Compatibility for PC, Mac, PS3, Xbox360',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/keyboard.jpg'
   },
   {
     name: 'Optical Mouse',
     price: 35,
     category: "Electronics",
     description: 'Wireless Computer Mouse with Nano Receiver',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/mouse.jpg'
   },
   {
     name: 'LED 27 inch monitor',
     price: 75,
     category: "Electronics",
     description: 'VIOTEK NB24CW 24-Inch LED Curved Monitor with Speakers, Bezel-Less Display, 75Hz 1080P Full-HD FreeSync VGA HDMI VESA - Xbox Ready (White)',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/monitor.jpg'
   },
   {
     name: 'Apple MacBook Air',
     price: 1500,
     category: "Electronics",
     description: 'Apple MacBook Air (13-inch Retina display, 1.6GHz dual-core Intel Core i5, 128GB) - Space Gray (Latest Model)',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/appleMacAir.jpg'
   },
   {
     name: 'Headphones',
     price: 90,
     category: "Over Ear Headphones",
     description: 'Upgraded Sony MDR 7506 Replacement Ear Pads by Wicked Cushions - Also Compatible with MDR V6 / MDR V7 / MDR CD900ST - Black',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/headphones.jpg'
   },
   {
     name: 'Office Desk',
     price: 5,
     category: "Office Furniture",
     description: 'SHW L-Shaped Home Office Corner Desk Wood Top, Walnut',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/officeDesk.jpg'
   },
   {
     name: 'Printer',
     price: 29.99,
     category: "Office Supplies",
     description: 'Canon Office Products PIXMA TS5020 BK Wireless color Photo Printer with Scanner & Copier, Black',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/printer.jpg'
   },
   {
     name: 'USB Connector',
     price: 15.99,
     category: "Apple Accessories",
     description: 'USB C Hub - USB Connector, 4 Ports Expander 3.0 Data Converter to Type C USB Hub Adapter for Macbook, MacBook Pro iMac, Google ChromeBook Pixelbook, XPS, Laptops, Samsung S9, S8 and More USB-C Devices',
-    img: 'https://www.elephantasticvegan.com/wp-content/uploads/2016/09/simple-vegan-chocolate-muffins-2.jpg'
+    image: '../images/USBConnector.jpg'
   }
 ]
